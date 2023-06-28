73242d061fe5c9627ab4ed67c00e3f6095cbd614
Willhelmatiq <rot3ad@gmail.com>
2023-06-28 15:40:54 +0200

    add postmapping

--- src/main/java/com/example/hs/loadbalancer/controller/FavouriteDrinkController.java
+++ src/main/java/com/example/hs/loadbalancer/controller/FavouriteDrinkController.java
@@ -8,10 +8,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.net.URI;
@@ -101,6 +98,68 @@ public class FavouriteDrinkController {
         }
         System.out.println(responseHeaders.getLocation());
         return new ResponseEntity<>(responseHeaders, HttpStatusCode.valueOf(302));
+    }
+
+    @PostMapping
+    public ResponseEntity<Drink> createNewDrink(@RequestBody Drink drink,
+                                                @RequestHeader(name = "Authorization") String headerParam,
+                                                HttpServletRequest request) {
+        if (!headerParam.equals(AuthorizationHeader)) {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN);
+        }
+        HttpHeaders responseHeaders = new HttpHeaders();
+        String partUrl = request.getServletPath();
+        if (requestNum % 3 == 0) {
+            requestNum++;
+            if (doHealthCheck(instance_1Ip)) {
+                responseHeaders.setLocation(URI.create(instance_1Ip.replace("host.docker.internal", "localhost") + partUrl));
+            } else {
+                if (doHealthCheck(instance_2Ip)) {
+                    responseHeaders.set("Location", instance_2Ip.replace("host.docker.internal", "localhost") + partUrl);
+                } else {
+                    if (doHealthCheck(instance_3Ip)) {
+                        responseHeaders.set("Location", instance_3Ip.replace("host.docker.internal", "localhost") + partUrl);
+                    } else {
+                        throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR);
+                    }
+                }
+            }
+        } else if (requestNum % 3 == 1){
+            requestNum++;
+            if (doHealthCheck(instance_2Ip)) {
+                responseHeaders.set("Location", instance_2Ip + partUrl);
+            } else {
+                if (doHealthCheck(instance_1Ip)) {
+                    responseHeaders.set("Location", instance_1Ip + partUrl);
+                } else {
+                    if (doHealthCheck(instance_3Ip)) {
+                        responseHeaders.set("Location", instance_3Ip.replace("host.docker.internal", "localhost") + partUrl);
+                    } else {
+                        throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR);
+                    }
+
+                }
+            }
+        } else {
+            requestNum = 0;
+            if (doHealthCheck(instance_3Ip)) {
+                responseHeaders.set("Location", instance_3Ip + partUrl);
+            } else {
+                if (doHealthCheck(instance_1Ip)) {
+                    responseHeaders.set("Location", instance_1Ip + partUrl);
+                } else {
+                    if (doHealthCheck(instance_2Ip)) {
+                        responseHeaders.set("Location", instance_2Ip.replace("host.docker.internal", "localhost") + partUrl);
+                    } else {
+                        throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR);
+                    }
+
+                }
+            }
+        }
+        System.out.println(responseHeaders.getLocation());
+        return new ResponseEntity<>(responseHeaders, HttpStatusCode.valueOf(302));
+
     }
 
     private boolean doHealthCheck(String instanceIp) {
